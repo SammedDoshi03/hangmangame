891347c79d79d4b580cef254b8ba64ee5a9a8270
SarthakDoshi04 <sarthakdoshi04@gmail.com>
2020-10-23 23:56:44 +0530

    Change log: 1. Added category list for selection of the word. 2. Added code to handle the selected category.

--- words.py
+++ words.py
@@ -1,4 +1,21 @@
-list_of_words = [
+category = [
+    "Christopher Nolan's movies",
+    "Flowers Names",
+    "Other Random Words"
+]
+
+movies_list = [
+    "Inception",
+    "Interstellar",
+    "Dunkirk",
+    "Tenet"
+]
+
+flowers_list = [
+    'Alstroemerias', 'Lilies', 'Carnations', 'Daisies', 'Orchids', 'Roses', 'Sunflowers', 'Tulips', 'Marigold'
+]
+
+other_list_of_words = [
     'FerreroRocher', 'Patchi',
     'horoscope', 'Cadbury',
     'lower', 'sleep',
